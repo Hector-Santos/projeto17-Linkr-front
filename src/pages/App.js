f1daf8fd125d415a632ddb06a1cc1c2be3bf46c5
Alex Souza <alex.webdevelloper@gmail.com>
2022-08-11 21:35:26 -0300

    Feat: Implementa ajustes no uso de link de uma postagem. Renderiza as hashtags em trending de forma dinâmica

--- src/pages/App.js
+++ src/pages/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Login />} />
           <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/hashtag/:hashtagName" element={<Home />} />
         </Routes>
       </TokenProvider>
     </BrowserRouter>
