9f098e360737e92ac4911b4e487463d2973ffef7
van-matos <vanmatos@poli.ufrj.br>
2022-08-12 17:44:06 -0300

    feat: display search results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { DebounceInput } from "react-debounce-input";
 import { AiOutlineSearch } from "react-icons/ai";
+import { DebounceInput } from "react-debounce-input";
 import axios from "axios";
 import dotenv from 'dotenv';
 import styled from "styled-components";
@@ -23,8 +23,17 @@ function SearchBar () {
         }
     }
 
+    function SearchResult ({ username, pictureUrl }) {
+        return (
+            <ResultsItem>
+                <img src={pictureUrl} alt={'img'} />
+                <h2>{username}</h2>
+            </ResultsItem>
+        )
+    }
+
     return (
-        <SearchContainer>
+        <Container>
             <SearchInput>
                 <DebounceInput
                     minLength = {3}
@@ -32,6 +41,8 @@ function SearchBar () {
                     onChange={e => {
                         if (e.target.value.length) {
                             userSearch(e.target.value);
+                        } else {
+                            setSearchResults([]);
                         }
                     }}
                     placeholder = "Search for people"
@@ -40,34 +51,45 @@ function SearchBar () {
                 />
                 <AiOutlineSearch className = "search-icon" />
             </SearchInput>
-        </SearchContainer>
+            <ResultsList>
+                {searchResults.length > 0 ? (
+                    searchResults.map((user, i) => {
+                        return <SearchResult
+                            pictureUrl={user.pictureUrl}
+                            username={user.username}
+                            key={i} />
+                    })
+                ) : ""}
+            </ResultsList>
+        </Container>
     )
 }
 
-const SearchContainer = styled.div`
+const Container = styled.div`    
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    
-    .search-icon {
-        width: 21px;
-        height: 21px;
-        margin-top: 13px;
-        margin-right: 17px;
-        position: absolute;
-        right: 0;
-        top: 0;
-        color: #C6C6C6;
-`
+    position: relative;
+`;
+
+const ResultsList = styled.div`
+    width: 100%;
+    max-height: 100vh;
+    padding-top: 45px;
+    border-radius: 8px;
+    background: #E7E7E7;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+`;
 
 const SearchInput = styled.div`
     width: 563px;
     height: 45px;
     border-radius: 8px;
     background: #FFFFFF;
-    position: relative;    
     display: flex;
+    position: relative;
 
     .searchBar-input {
         width: 100%;
@@ -76,20 +98,52 @@ const SearchInput = styled.div`
         border-radius: 8px;
 
         font-family: 'Lato';
-        font-style: normal;
         font-weight: 400;
         font-size: 19px;
         line-height: 23px;
         
         &::placeholder {
             font-family: 'Lato';
-            font-style: normal;
             font-weight: 400;
             font-size: 19px;
             line-height: 23px;
             color: #C6C6C6;
         }
     }
-`
+
+    .search-icon {
+        width: 21px;
+        height: 21px;
+        margin-top: 13px;
+        margin-right: 17px;
+        color: #C6C6C6;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+`;
+
+const ResultsItem = styled.div`
+    padding-bottom: 10px;
+    padding-top: 10px;
+    display: flex;
+    align-items: center;
+ 
+    img {
+        width: 39px;
+        height: 39px;
+        margin-left: 15px;
+        border-radius: 50%;
+    }
+  
+    h2 {
+        margin-left: 15px;
+        font-family: 'Lato';
+        font-weight: 400;
+        font-size: 19px;
+        line-height: 23px;
+        color: #515151;
+    }
+`;
 
 export default SearchBar;
